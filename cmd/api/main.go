7c16e2f4dd4115599f38b2c352f3c547a526419d
Aryan C <aryanc1027@gmail.com>
2024-11-12 13:27:27 -0500

    Update Module Structure

---
+++ cmd/api/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+    "github.com/aryanc1027/api-kafka-golang/internal/api"
+    "github.com/aryanc1027/api-kafka-golang/internal/config"
+    "github.com/aryanc1027/api-kafka-golang/internal/kafka"
+    "github.com/aryanc1027/api-kafka-golang/pkg/logger"
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aryanc1027/api-kafka-golang
+
+go 1.23.3
